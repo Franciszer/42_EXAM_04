5a43971053c15343265274271a00663197a4f712
Francois THIERREE <frthierr@e2r11p16.42.fr>
2021-03-30 14:16:50 +0200

    some progress

--- srcs/microshell.c
+++ srcs/microshell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   microshell.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: francisco <francisco@student.42.fr>        +#+  +:+       +#+        */
+/*   By: frthierr <frthierr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/04 09:56:29 by frthierr          #+#    #+#             */
-/*   Updated: 2021/03/29 21:02:02 by francisco        ###   ########.fr       */
+/*   Updated: 2021/03/30 12:23:18 by frthierr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,8 +35,27 @@ void	handle_error_fatal(const char *str, const char exit_status) {
 	exit (exit_status);	
 }
 
+void	free_args(char **args, const int max_idx) {
+	for (int i = 0; i < max_idx ; i++)
+		free(args[i]);
+	free(args);
+}
+
+void	fatal_free_args(char **args, const int max_idx) {
+	handle_error("error: cannot execute ");
+	handle_error(args[i]);
+	free_args(args, max_idx);
+	exit (1);
+}
+
 bool	find_next(t_command_handler *current) {
-	int		i = current->begin;
+	int		i;
+	if (SET(current->next_pipe))
+		i = current->next_pipe;
+	else if (SET(current->next_sep))
+		i = current->next_sep;
+	else
+		i = current->begin;
 	bool	valid = false;
 	current->next_sep = NOT_SET;
 	current->next_pipe = NOT_SET;
@@ -53,21 +72,62 @@ bool	find_next(t_command_handler *current) {
 	while (current->data[++i]) {
 		if (!strcmp(PIPE, current->data[i])) {
 				current->next_pipe = i;
+				current->nb_args = current->next_pipe - current->begin;
 				return true;
 			}
 		else if (!strcmp(SEP, current->data[i])) {
 			current->next_sep = i;
+			current->end_args = current->next_sep - current->begin;
 			return true;
 		}
 	}
+	current->end_args = i - current->begin;
 	return true;
 }
 
+void	bt_cd(const t_command_handler *current) {
+	if (current->end_args != 1) {
+			FATAL_CD_BAD_ARGS;
+		}
+	if (chdir(current->data[current->begin + 1]) == -1)
+		FATAL_CD_CHDIR;
+}
 
+void	exec_pipeless(const t_command_handler *current) {
+	pid_t	pid;
+
+	if (!INIT_PIPE(pid))
+		FATAL_SYSCALL;
+	else if (CHILD(pid)) {
+		if (execve(current->data[current->begin], args, current->env_vars) == -1)
+			fatal_free_args();
+	}
+	else {
+		waitpid(pid, NULL, 0);
+	}
+}
+
+void	exec_command(const t_command_handler *current) {
+	char **args;
+		
+
+	if (!(args = malloc(sizeof(char*) * ((current->end_args - current->begin) + 1))))
+		FATAL_SYSCALL;
+	for (int i = current->begin; i < current->end_args ; i++) {
+		if (!(args[i - current->begin] = malloc\
+			(sizeof(char) * (ft_strlen(current->data[i]) + 1)))
+				fatal_free_args(args, i);
+		strcpy(args[i - current->begin], current->args[i]);
+	}
+	if (!SET(current->next_pipe)) {
+		
+	}
+}
 
 void	microshell(const char **args, const char **env_vars) {
 	t_command_handler	command_handler;
 	
 	INIT_COMMAND_HANDLER(command_handler, args, env_vars);
-	find_next(&command_handler);
+	while(find_next(&command_handler)) exec_command(&command_handler);
+	return 0;
 }
--- srcs/microshell.h
+++ srcs/microshell.h
@@ -6,7 +6,7 @@
 /*   By: frthierr <frthierr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/04 09:57:22 by frthierr          #+#    #+#             */
-/*   Updated: 2021/03/29 16:06:16 by frthierr         ###   ########.fr       */
+/*   Updated: 2021/03/30 12:22:21 by frthierr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,18 @@
 # define PIPE 		"|"
 # define SEP 		";"
 # define NOT_SET	-1
+# define SET(x)		x != NOT_SET
+# define INIT_PIPE(pid) if ((pid = fork()) != -1);
+
+# define ERR_RET 			1
+# define ERR_CD_BAD_ARGS	"error: cd: bad arguments"
+# define FATAL_CD_BAD_ARGS	handle_error_fatal(ERR_CD_BAD_ARGS, ERR_RET)
+# define FATAL_CD_CHDIR		handle_error("error: cd: cannot change directory to ");\
+								handle_error_fatal(current->data[current->begin + 1], ERR_RET)
+# define FATAL_SYSCALL		handle_error_fatal("error: fatal", 1)
+
+# define CHILD(pid)			pid == 0
+# define PARENT(pid)		pid != 0
 
 typedef struct	s_command_handler {
 	const char	**data;
@@ -35,6 +47,7 @@ typedef struct	s_command_handler {
 	int			begin;
 	int			next_pipe;
 	int			next_sep;
+	int			end_args;
 }				t_command_handler;
 
 # define INIT_COMMAND_HANDLER(name, args, env_vars) \
@@ -42,7 +55,8 @@ typedef struct	s_command_handler {
 	name.begin = NOT_SET;\
 	name.next_pipe = NOT_SET;\
 	name.next_sep = NOT_SET;\
-	name.env = (const char**)env_vars;
+	name.env = (const char**)env_vars;\
+	name.end_args = NOT_SET
 
 void	microshell(const char **args, const char **env_vars);
 
@@ -58,6 +72,17 @@ void	handle_error_fatal(const char *str, const char exit_status);
 
 bool	find_next(t_command_handler *current);
 
-void	exec_current(t_command_handler *current);
+void	exec_command(const t_command_handler *current);
+
+void	exec_pipe(const t_command_handler *current);
+
+void	exec_pipeless(const t_command_handler *current);
+
+void	bt_cd(const t_command_handler *current);
+
+void	free_args(char **args, const int max_idx);
+
+void	fatal_free_args(char **args, const int max_idx);
+
 
 #endif
